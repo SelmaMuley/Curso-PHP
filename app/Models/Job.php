6c2caabd1676e021c4e067f617f82989b6771269
Selma Muley Salas <selma.muley@gmail.com>
2019-09-29 21:46:37 +0200

    Configurando la base de datos para añadir datos

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -3,12 +3,17 @@ namespace App\Models;
 //comentario de prueba
 //require_once 'BaseElement.php';
 
-class Job extends BaseElement {
-    public function __construct($title, $description) {
+use Illuminate\Database\Eloquent\Model;
+
+class Job extends Model
+{
+
+    protected $table = 'jobs';
+    /*public function __construct($title, $description) {
         $newTitle = 'Job: ' . $title;
         $this->title = $newTitle;
         $this->description=$description;
-    }
+    }*/
 
     public function getDurationAsString() {
         $years = floor($this->months / 12);
